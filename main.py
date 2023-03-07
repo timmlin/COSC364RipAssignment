b65739752bf5fe6886ad3980fe69dcb3e4397efc
Tim Lindbom <tli89@cs18259kq.canterbury.ac.nz>
2023-03-08 10:59:52 +1300

    initilises the classes

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-from routerConfig import *
+#Main RIP protocol file
+#Tim Lindbom & Ben Ireland
+#8/3/23
+
+import routerConfig
+from router import *
+
 
 
 def main():
-    config = readFile("Routers.txt")
-    for router in config:
-        getInfo(router)
+    config = routerConfig.readFile("Routers.txt")
+    defaultRouterName = "router"
+    routersList = []
+    #initilises a router class for each router in the file 
+    for strRouter in config:
 
-main()
+        currentRouter = routerConfig.getInfo(strRouter)
+        routername = f"{defaultRouterName}{currentRouter[0]}"
+        #print(routername, currentRouter)
+
+        routersList.append(Router(currentRouter))
+        
+
+    for router in routersList:
+        router.PrintParams()
+
+
+
+
+if __name__ == "__main__":
+
+    main()
+
+
