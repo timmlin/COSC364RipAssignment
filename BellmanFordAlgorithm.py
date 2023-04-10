bebd06b2852e212d6638ed789bfacf69d958d232
bir16 <bir16@uclive.ac.nz>
2023-04-10 15:24:26 +1200

    Finish UpdateRoute function

--- BellmanFordAlgorithm.py
+++ BellmanFordAlgorithm.py
@@ -15,8 +15,9 @@ def ComputeRoutingAlgorithm(hostRouter, peerRouterID, peerRouterEntries):
         entryMetric = entry[1]
         metric = min(entryMetric + linkCost, 16)
 
+        #Checks if router is in the table. If not add the router to table.
         entryID = entry[0]
-        if entryID not in hostRouter.routingTable:     #Checks if router is in the table. If not add the router to table.
+        if entryID not in hostRouter.routingTable:    
             AddNewRoute(hostRouter, peerRouterID, entryID, metric)
         else:
             UpdateRoute(hostRouter, peerRouterID, entryID, metric)
@@ -26,9 +27,26 @@ def ComputeRoutingAlgorithm(hostRouter, peerRouterID, peerRouterEntries):
 def AddNewRoute(hostRouter, peerRouterID, entryID, metric):
     """Adds a new route to the routing table if the entry does not exist"""
     if metric <= 16:
-        routeChangeFlag = True
+        routeChangeFlag = 0
         hostRouter.routingTable[entryID] = [metric, peerRouterID, routeChangeFlag]
-        InitRouteTimers() # yet to be implemented
+        # InitRouteTimers() # yet to be implemented
 
-def UpdateRoute(hostRouter, peerRouterID, entryID, metric):
+
+def UpdateRoute(hostRouter, peerRouterID, entryID, newMetric):
     """Updates the current route if ir has been changed and resets timers."""
+    currentNextHopID = hostRouter.routingList.get(entryID)[1]
+    currentMetric = hostRouter.routingList.get(entryID)[0]
+    if currentNextHopID == peerRouterID:
+        #ResetTimeout() # yet to be implemented
+        if newMetric >= 16:
+            i = 0
+            #InitDeletion()
+        elif newMetric != currentMetric:
+            hostRouter.routingList[entryID] = [newMetric, currentNextHopID, 0]
+    else:
+        if newMetric >= 16:
+            i = 0
+            #InitDeletion()
+        elif newMetric < currentMetric:
+            hostRouter.routingList[entryID] = [newMetric, peerRouterID, 0]
+            
