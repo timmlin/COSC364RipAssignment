5d3385cb67911eeb345235ef02373d5917daebb9
bir16 <bir16@uclive.ac.nz>
2023-04-10 15:51:49 +1200

    Test BellmanFord functionality, Working as expected

--- BellmanFordAlgorithm.py
+++ BellmanFordAlgorithm.py
@@ -1,13 +1,13 @@
 #Bellman Ford Algorithm for use in a RIP routing protocol
 #Tim Lindbom & Ben Ireland 
 #23/2/23
-
+from Router import *
 
 def ComputeRoutingAlgorithm(hostRouter, peerRouterID, peerRouterEntries):
     """Computes RIP routing algorithm and updates the routing table"""
     for entry in peerRouterEntries: 
         # Match the Peer Router ID to one of the IDs in the output list to get the cost of the link 
-        for output in hostRouter.outputList:
+        for output in hostRouter.outputs:
             outputRouterID = output[2] 
             outputCost = output[1]
             if outputRouterID == peerRouterID:
@@ -26,7 +26,7 @@ def ComputeRoutingAlgorithm(hostRouter, peerRouterID, peerRouterEntries):
 
 def AddNewRoute(hostRouter, peerRouterID, entryID, metric):
     """Adds a new route to the routing table if the entry does not exist"""
-    if metric <= 16:
+    if metric < 16:
         routeChangeFlag = 0
         hostRouter.routingTable[entryID] = [metric, peerRouterID, routeChangeFlag]
         # InitRouteTimers() # yet to be implemented
@@ -34,19 +34,29 @@ def AddNewRoute(hostRouter, peerRouterID, entryID, metric):
 
 def UpdateRoute(hostRouter, peerRouterID, entryID, newMetric):
     """Updates the current route if ir has been changed and resets timers."""
-    currentNextHopID = hostRouter.routingList.get(entryID)[1]
-    currentMetric = hostRouter.routingList.get(entryID)[0]
+    currentNextHopID = hostRouter.routingTable.get(entryID)[1]
+    currentMetric = hostRouter.routingTable.get(entryID)[0]
     if currentNextHopID == peerRouterID:
         #ResetTimeout() # yet to be implemented
         if newMetric >= 16:
             i = 0
             #InitDeletion()
         elif newMetric != currentMetric:
-            hostRouter.routingList[entryID] = [newMetric, currentNextHopID, 0]
+            hostRouter.routingTable[entryID] = [newMetric, currentNextHopID, 0]
     else:
         if newMetric >= 16:
             i = 0
             #InitDeletion()
         elif newMetric < currentMetric:
-            hostRouter.routingList[entryID] = [newMetric, peerRouterID, 0]
-            
+            hostRouter.routingTable[entryID] = [newMetric, peerRouterID, 0]
+
+# router_id 0, inputs 701 702 777, outputs 5000-1-1 5002-5-4
+router1 = Router([0, [701, 702, 777], [[5000, 1, 1], [5002, 5, 4]]])
+ComputeRoutingAlgorithm(router1, 1, [[1, 0], [3, 3]])
+router1.PrintParams()
+ComputeRoutingAlgorithm(router1, 4, [[4, 0], [3, 2]])
+router1.PrintParams()
+
+UpdateRoute(router1, 4, 3, 1)
+router1.PrintParams()
+
